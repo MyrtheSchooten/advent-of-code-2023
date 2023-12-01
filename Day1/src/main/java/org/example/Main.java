d1a48b7f6db4bcc1e81283bf6849bf2d78a09c87
Myrthe <myrtheschooten1@gmail.com>
2023-12-01 11:25:34 +0100

    Setup and day 1 solutions

---
+++ Day1/src/main/java/org/example/Main.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int solution = Solver.solve();
+        System.out.println("Day 1 solution is: " + solution);
+    }
+}
